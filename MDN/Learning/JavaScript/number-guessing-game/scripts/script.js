7d6a682e248338a4d97f0c4a5435a7d7681fea4a
blackdogcode <blackdogcode@gmail.com>
2020-01-27 14:47:21 +0900

    Play guessing number

---
+++ MDN/Learning/JavaScript/number-guessing-game/scripts/script.js
@@ -0,0 +1,11 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
