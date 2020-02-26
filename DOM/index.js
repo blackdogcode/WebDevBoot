649da4ed446839b80d543081cd5cc47c27ff364c
blackdogcode <blackdogcode@gmail.com>
2020-02-26 20:29:32 +0900

    DOM Javascript Manipulation demo code

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,8 @@
-alert("Hello World");
-document.querySelector("h1").innerHTML="Hello, Liam Song";
+setTimeout(function(){
+  alert("Document Changed!");
+  // change list text attribute
+  var lists = document.body.querySelector("ul");
+  lists.lastElementChild.innerHTML = "Liam, Song";
+  // change list link color
+  document.querySelector(".list a").style.color = "#c70d3a";
+}, 3000);
