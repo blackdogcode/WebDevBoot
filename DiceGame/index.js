92d1375b266c812b47840b1c16833a6492e4a1b7
blackdogcode <blackdogcode@gmail.com>
2020-02-27 21:43:18 +0900

    DiceGame Final Version

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,1 +1,18 @@
-alert("Hello Dice Game");
+// random dice number geneartor [1-6];
+var i = Math.floor(Math.random() * 6) + 1;
+var j = Math.floor(Math.random() * 6) + 1;
+
+// change players dice images given by random dice number
+document.getElementsByClassName("img1")[0].setAttribute("src", "./images/dice" + i + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "./images/dice" + j + ".png");
+
+// show winner or draw
+if (i > j) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (i == j) {
+  document.querySelector("h1").innerHTML = "Draw";
+}
+else {
+  document.querySelector("h1").innerHTML = "Play 2 Wins! 🚩";
+}
